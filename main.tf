8bc51bd51431f78a895dece7ee0e652ce9f5a0f1
samuelmcmanus819 <samuelmcmanus819@gmail.com>
2025-11-11 16:15:06 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -20,7 +20,7 @@ module "elastic_waf_destination" {
     module.waf_wrapper_asset_essentials_dev_us_east_1.replication_role_arn,
     module.waf_wrapper_asset_essentials_prod_us_east_1.replication_role_arn,
     module.waf_wrapper_asset_essentials_prod_eu_west_2.replication_role_arn,
-    module.waf_wrapper_asset_essentials_prod_ca_central_1.replication_role_arn,
+    module.waf_wrapper_asset_essentials_prod_ca_central_1.replication_role_arn
   ]
 }
 ###############################################################################
