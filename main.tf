ed22684398ada1cf2d53e232a6b1265093ceca24
samuelmcmanus819 <samuelmcmanus819@gmail.com>
2025-11-11 16:21:30 -0600

    Update main.tf

--- main.tf
+++ main.tf
@@ -18,7 +18,7 @@ module "elastic_waf_destination" {
     module.waf_wrapper_confirm_uk_prod_eu_west_2.replication_role_arn,
     module.waf_wrapper_confirm_uk_prod_us_east_2.replication_role_arn,
     module.waf_wrapper_asset_essentials_dev_us_east_1.replication_role_arn,
-    module.waf_wrapper_evm_legacy_cloudfront.replication_role_arn,,
+    module.waf_wrapper_evm_legacy_cloudfront.replication_role_arn,
     module.waf_wrapper_asset_essentials_prod_us_east_1.replication_role_arn,
     module.waf_wrapper_asset_essentials_prod_eu_west_2.replication_role_arn,
     module.waf_wrapper_asset_essentials_prod_ca_central_1.replication_role_arn
