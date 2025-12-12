67b35b5ac66d4b6c53c510a7cd6e92aaef7b2983
samuelmcmanus819 <samuelmcmanus819@gmail.com>
2025-12-12 13:14:33 -0600

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -13,7 +13,7 @@ locals {
     "us-east-1"       = "arn:aws:s3:::aw-ue1-ob1-alloy-audit-logs"
     "us-east-2"       = "arn:aws:s3:::aw-ue1-ob1-alloy-audit-logs"
     "eu-west-2"       = "arn:aws:s3:::aw-ew2-ob1-alloy-audit-logs"
-    "ap-south-1"      = "arn:aws:s3:::aw-ue1-ob1-alloy-audit-logs"
+    "ap-south-1"      = "arn:aws:s3:::aw-as2-ob1-alloy-audit-logs"
     "ap-southeast-2"  = "arn:aws:s3:::aw-as2-ob1-alloy-audit-logs"
     "ca-central-1"    = "arn:aws:s3:::aw-ue1-ob1-alloy-audit-logs"
   }
