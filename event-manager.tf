4e8ef121ac9acbbd39890278ccb87ccc640df33d
samuelmcmanus819 <samuelmcmanus819@gmail.com>
2026-01-12 14:49:44 -0600

    Update event-manager.tf

--- event-manager.tf
+++ event-manager.tf
@@ -1,5 +1,5 @@
 provider "aws" {
-  alias  = "evm_dev_us_east_1"
+  alias  = "event_manager_dev_us_east_1"
   region = "us-east-1"
   assume_role {
     role_arn = "arn:aws:iam::993748385324:role/WAF_Provisioner"
@@ -7,7 +7,7 @@ provider "aws" {
 }
 
 provider "aws" {
-  alias  = "evm_prod_us_east_1"
+  alias  = "event_manager_prod_us_east_1"
   region = "us-east-1"
   assume_role {
     role_arn = "arn:aws:iam::622913004475:role/WAF_Provisioner"
@@ -18,21 +18,21 @@ provider "aws" {
 #########################################################################
 ########################### Firehose IAM Role ###########################
 #########################################################################
-module "firehose_role_policy_evm_dev" {
+module "firehose_role_policy_event_manager_dev" {
   source = "./modules/firehose-role-policy"
 
   providers = {
-    aws = aws.evm_dev_us_east_1
+    aws = aws.event_manager_dev_us_east_1
   }
 
   s3_bucket_arns = values(local.alloy_s3_buckets)
 }
 
-module "firehose_role_policy_evm_prod" {
+module "firehose_role_policy_event_manager_prod" {
   source = "./modules/firehose-role-policy"
 
   providers = {
-    aws = aws.evm_prod_us_east_1
+    aws = aws.event_manager_prod_us_east_1
   }
 
   s3_bucket_arns = values(local.alloy_s3_buckets)
@@ -42,46 +42,46 @@ module "firehose_role_policy_evm_prod" {
 ########################### EVM Dev Modules #############################
 #########################################################################
 
-module "waf_wrapper_evm_dev_us_east_1" {
-  depends_on = [ module.firehose_role_policy_evm_dev ]
+module "waf_wrapper_event_manager_dev_us_east_1" {
+  depends_on = [ module.firehose_role_policy_event_manager_dev ]
   source = "./modules/waf-wrapper"
 
   providers = {
-    aws = aws.evm_dev_us_east_1
+    aws = aws.event_manager_dev_us_east_1
   }
 
   # Key = environments/<subpath>/<filename>
-  alb_arns                    = local.environments["evm-dev-us-east-1"].alb_arns
-  api_gateway_ids             = local.environments["evm-dev-us-east-1"].api_gateway_ids
-  disabled_rules              = local.environments["evm-dev-us-east-1"].disabled_rules
-  environment                 = "evm-dev-us-east-1"
-  firehose_destination        = local.alloy_s3_buckets[local.environments["evm-dev-us-east-1"].region]
-  firehose_role_arn           = module.firehose_role_policy_evm_dev.firehose_role_arn
-  global                      = local.environments["evm-dev-us-east-1"].global
+  alb_arns                    = local.environments["event-manager-dev-us-east-1"].alb_arns
+  api_gateway_ids             = local.environments["event-manager-dev-us-east-1"].api_gateway_ids
+  disabled_rules              = local.environments["event-manager-dev-us-east-1"].disabled_rules
+  environment                 = "event-manager-dev-us-east-1"
+  firehose_destination        = local.alloy_s3_buckets[local.environments["event-manager-dev-us-east-1"].region]
+  firehose_role_arn           = module.firehose_role_policy_event_manager_dev.firehose_role_arn
+  global                      = local.environments["event-manager-dev-us-east-1"].global
   log_forward_destination     = "arn:aws:s3:::bsw-siem-waf"
-  protection_rules            = local.environments["evm-dev-us-east-1"].protection_rules
+  protection_rules            = local.environments["event-manager-dev-us-east-1"].protection_rules
 }
 
 #########################################################################
 ########################### EVM Prod Modules ############################
 #########################################################################
 
-module "waf_wrapper_evm_prod_us_east_1" {
-  depends_on = [ module.firehose_role_policy_evm_prod ]
+module "waf_wrapper_event_manager_prod_us_east_1" {
+  depends_on = [ module.firehose_role_policy_event_manager_prod ]
   source = "./modules/waf-wrapper"
 
   providers = {
-    aws = aws.evm_prod_us_east_1
+    aws = aws.event_manager_prod_us_east_1
   }
 
   # Key = environments/<subpath>/<filename>
-  alb_arns                    = local.environments["evm-prod-us-east-1"].alb_arns
-  api_gateway_ids             = local.environments["evm-prod-us-east-1"].api_gateway_ids
-  disabled_rules              = local.environments["evm-prod-us-east-1"].disabled_rules
-  environment                 = "evm-prod-us-east-1"
-  firehose_destination        = local.alloy_s3_buckets[local.environments["evm-prod-us-east-1"].region]
-  firehose_role_arn           = module.firehose_role_policy_evm_prod.firehose_role_arn
-  global                      = local.environments["evm-prod-us-east-1"].global
+  alb_arns                    = local.environments["event-manager-prod-us-east-1"].alb_arns
+  api_gateway_ids             = local.environments["event-manager-prod-us-east-1"].api_gateway_ids
+  disabled_rules              = local.environments["event-manager-prod-us-east-1"].disabled_rules
+  environment                 = "event-manager-prod-us-east-1"
+  firehose_destination        = local.alloy_s3_buckets[local.environments["event-manager-prod-us-east-1"].region]
+  firehose_role_arn           = module.firehose_role_policy_event_manager_prod.firehose_role_arn
+  global                      = local.environments["event-manager-prod-us-east-1"].global
   log_forward_destination     = "arn:aws:s3:::bsw-siem-waf"
-  protection_rules            = local.environments["evm-prod-us-east-1"].protection_rules
+  protection_rules            = local.environments["event-manager-prod-us-east-1"].protection_rules
 }
