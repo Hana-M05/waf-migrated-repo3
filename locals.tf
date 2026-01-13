bf7ecb02519b975dc08c95a5361d310cd1153945
samuelmcmanus819 <samuelmcmanus819@gmail.com>
2026-01-13 11:21:50 -0600

    Update locals.tf

--- locals.tf
+++ locals.tf
@@ -9,7 +9,15 @@ locals {
     replace(replace(config_file, "/", "-"), ".yaml", "") => yamldecode(file("${path.module}/environments/${config_file}"))
   }
 
-  devsecops_monitor_emails = ["security@brightlysoftware.com", "productops@brightlysoftware.com"]
+  devsecops_monitor_emails = [
+    "security@brightlysoftware.com", 
+    "productops@brightlysoftware.com",
+    "amritpal.singh@brightlysoftware.com",
+    "deepak.nagvanshi@brightlysoftware.com",
+    "tyler.bassett@brightlysoftware.com",
+    "benjamin.issa@siemens.com",
+    "amanda.reams@brightlysoftware.com"
+  ]
 
   alloy_s3_buckets = {
     "us-east-1"      = "arn:aws:s3:::aw-ue1-ob1-alloy-audit-logs"
