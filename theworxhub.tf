f5492a11dc66f5b426c4c3188c41d91b31193e89
samuelmcmanus819 <samuelmcmanus819@gmail.com>
2026-01-21 13:38:03 -0600

    Update theworxhub.tf

--- theworxhub.tf
+++ theworxhub.tf
@@ -65,6 +65,7 @@ module "waf_wrapper_theworxhub_dev_us_east_1" {
   global                  = local.environments["theworxhub-dev-us-east-1"].global
   log_forward_destination = "arn:aws:s3:::bsw-siem-waf"
   protection_rules        = local.environments["theworxhub-dev-us-east-1"].protection_rules
+  waf_error_subscribers   = local.environments["theworxhub-dev-us-east-1"].waf_error_subscribers
 }
 
 ###########################################################################
@@ -88,4 +89,5 @@ module "waf_wrapper_theworxhub_prod_us_east_1" {
   global                  = local.environments["theworxhub-prod-us-east-1"].global
   log_forward_destination = "arn:aws:s3:::bsw-siem-waf"
   protection_rules        = local.environments["theworxhub-prod-us-east-1"].protection_rules
+  waf_error_subscribers   = local.environments["theworxhub-prod-us-east-1"].waf_error_subscribers
 }
