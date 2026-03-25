d252c2ea68182fb89e499054c0d7871139e94546
samuelmcmanus819 <samuelmcmanus819@gmail.com>
2026-03-25 07:59:18 -0500

    deduplicate waf

--- theworxhub.tf
+++ theworxhub.tf
@@ -69,29 +69,6 @@ module "waf_wrapper_theworxhub_dev_us_east_1" {
   waf_error_subscribers   = local.environments["theworxhub-dev-us-east-1"].waf_error_subscribers
 }
 
-module "waf_wrapper_theworxhub_sandbox_orange_us_east_1" {
-  depends_on = [module.firehose_role_policy_theworxhub_dev]
-  source     = "./modules/waf-wrapper"
-
-  providers = {
-    aws = aws.theworxhub_dev_us_east_1
-  }
-
-  # Key = environments/<subpath>/<filename>
-  alb_arns                = local.environments["theworxhub-sandbox-orange-us-east-1"].alb_arns
-  api_gateway_ids         = local.environments["theworxhub-sandbox-orange-us-east-1"].api_gateway_ids
-  disabled_rules          = local.environments["theworxhub-sandbox-orange-us-east-1"].disabled_rules
-  environment             = "theworxhub-sandbox-orange-us-east-1"
-  firehose_destination    = local.alloy_s3_buckets[local.environments["theworxhub-sandbox-orange-us-east-1"].region]
-  firehose_role_arn       = module.firehose_role_policy_theworxhub_dev.firehose_role_arn
-  global                  = local.environments["theworxhub-sandbox-orange-us-east-1"].global
-  log_forward_destination = "arn:aws:s3:::bsw-siem-waf"
-  protection_rules        = local.environments["theworxhub-sandbox-orange-us-east-1"].protection_rules
-  redacted_headers        = local.environments["theworxhub-sandbox-orange-us-east-1"].redacted_headers
-  waf_error_subscribers   = local.environments["theworxhub-sandbox-orange-us-east-1"].waf_error_subscribers
-
-}
-
 
 ###########################################################################
 ######################### TheWorxHub Prod Modules #########################
