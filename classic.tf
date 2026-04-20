4e26f3a88be5fe63a9595c4c7ab4c1c763816baa
samuelmcmanus819 <samuelmcmanus819@gmail.com>
2026-04-20 12:27:06 -0500

    Update Classic WAF to enforce in staging

--- classic.tf
+++ classic.tf
@@ -72,6 +72,28 @@ module "waf_wrapper_classic_dev_us_east_2" {
 #########################################################################
 ######################### Classic Prod Modules ##########################
 #########################################################################
+module "waf_wrapper_classic_staging_us_east_2" {
+  depends_on = [module.firehose_role_policy_classic_prod]
+  source = "./modules/waf-wrapper"
+
+  providers = {
+    aws = aws.classic_prod_us_east_2
+  }
+
+  # Key = environments/<subpath>/<filename>
+  alb_arns                    = local.environments["classic-staging-us-east-2"].alb_arns
+  api_gateway_ids             = local.environments["classic-staging-us-east-2"].api_gateway_ids
+  disabled_rules              = local.environments["classic-staging-us-east-2"].disabled_rules
+  environment                 = "classic-staging-us-east-2"
+  firehose_destination        = local.alloy_s3_buckets[local.environments["classic-staging-us-east-2"].region]
+  firehose_role_arn           = module.firehose_role_policy_classic_prod.firehose_role_arn
+  global                      = local.environments["classic-staging-us-east-2"].global
+  log_forward_destination     = "arn:aws:s3:::bsw-siem-waf"
+  protection_rules            = local.environments["classic-staging-us-east-2"].protection_rules
+  redacted_headers            = local.environments["classic-staging-us-east-2"].redacted_headers
+  waf_error_subscribers       = local.environments["classic-staging-us-east-2"].waf_error_subscribers
+}
+
 module "waf_wrapper_classic_prod_us_east_2" {
   depends_on = [module.firehose_role_policy_classic_prod]
   source = "./modules/waf-wrapper"
